4a8823005504d409282795d517b82523f4ae35ce
Juganor <114257207+Juganor@users.noreply.github.com>
2022-09-23 14:08:18 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,112 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # . .
+        . # # # .
+        `)
+    basic.clearScreen()
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . # # # .
+        # # # # #
+        `)
+    basic.clearScreen()
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        # # # # #
+        . . . . .
+        `)
+    basic.clearScreen()
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . . . . .
+        . . . . .
+        `)
+    basic.clearScreen()
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.clearScreen()
+    basic.showLeds(`
+        # # # # #
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.clearScreen()
+})
+basic.forever(function () {
+    while (input.buttonIsPressed(Button.AB)) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.clearScreen()
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+        basic.clearScreen()
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.clearScreen()
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+        basic.clearScreen()
+        basic.pause(100)
+    }
+    while (input.buttonIsPressed(Button.A)) {
+        basic.showLeds(`
+            . . # . .
+            . # # . .
+            # # # # #
+            . # # . .
+            . . # . .
+            `)
+        basic.clearScreen()
+        basic.pause(500)
+    }
+    while (input.buttonIsPressed(Button.B)) {
+        basic.showLeds(`
+            . . # . .
+            . . # # .
+            # # # # #
+            . . # # .
+            . . # . .
+            `)
+        basic.clearScreen()
+        basic.pause(500)
+    }
+})
